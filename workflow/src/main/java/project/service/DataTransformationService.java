4724d763c608e9272a95dfaf8e7a6def4eeec786
Mathias Santos Reis <mathias.santos-reis@etu.univ-cotedazur.fr>
2025-02-14 10:22:59 +0100

    nouveaux filtre et possibilité d'avoir 2 flux en input

--- workflow/src/main/java/project/service/DataTransformationService.java
+++ workflow/src/main/java/project/service/DataTransformationService.java
@@ -1,6 +1,9 @@
-package src.main.java.project.service;
+package project.service;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import project.model.UserRecord;
 
 public class DataTransformationService {
@@ -34,5 +37,46 @@ public class DataTransformationService {
                         user.city()
                 ))
                 .collect(Collectors.toList());
+    }
+    // Filtrer les utilisateurs par genre (Male/Female)
+    public static List<UserRecord> filterByGender(List<UserRecord> users, String gender) {
+        return users.stream()
+                .filter(user -> user.gender().equalsIgnoreCase(gender))
+                .collect(Collectors.toList());
+    }
+
+    // Filtrer les utilisateurs par pays
+    public static List<UserRecord> filterByCountry(List<UserRecord> users, String country) {
+        return users.stream()
+                .filter(user -> user.country().equalsIgnoreCase(country))
+                .collect(Collectors.toList());
+    }
+
+    // Filtrer par domaine d'email (@gmail.com, @yahoo.com, etc.)
+    public static List<UserRecord> filterByEmailDomain(List<UserRecord> users, String domain) {
+        return users.stream()
+                .filter(user -> user.email().endsWith(domain))
+                .collect(Collectors.toList());
+    }
+
+    // Calculer la moyenne d'âge
+    public static double calculateAverageAge(List<UserRecord> users) {
+        return users.stream()
+                .mapToInt(UserRecord::age)
+                .average()
+                .orElse(0.0);
+    }
+
+    // Compter le nombre d’utilisateurs par ville
+    public static Map<String, Long> countUsersByCity(List<UserRecord> users) {
+        return users.stream()
+                .collect(Collectors.groupingBy(UserRecord::city, Collectors.counting()));
+    }
+
+    // Fusionner deux flux et calculer la moyenne d'âge combinée
+    public static double mergeAndCalculateAverageAge(List<UserRecord> users1, List<UserRecord> users2) {
+        List<UserRecord> mergedList = Stream.concat(users1.stream(), users2.stream())
+                .collect(Collectors.toList());
+        return calculateAverageAge(mergedList);
     }
 }
---
+++ workflow/src/main/java/project/service/MergeNodeService.java
@@ -0,0 +1,17 @@
+package project.service;
+
+import project.model.UserRecord;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MergeNodeService {
+
+    // Fusionner deux flux de données
+    public static List<UserRecord> mergeUserStreams(List<UserRecord> stream1, List<UserRecord> stream2) {
+        return Stream.concat(stream1.stream(), stream2.stream())
+                .distinct() // pas de doublons
+                .collect(Collectors.toList());
+    }
+}
