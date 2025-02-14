c5e0220da5552e4d6a5eead6b5d5563f30b2c4f7
Rom1420 <romain.abbonato@etu.univ-cotedazur.fr>
2025-02-14 09:41:22 +0100

    up

--- workflow/src/main/java/project/model/UserRecord.java
+++ workflow/src/main/java/project/model/UserRecord.java
@@ -1,4 +1,14 @@
 package project.model;
 
-public record UserRecord() {
-}
+public record UserRecord(
+        int id,
+        String firstName,
+        String lastName,
+        String email,
+        String gender,
+        int age,
+        String birthdate,
+        String country,
+        String city
+) {}
+
