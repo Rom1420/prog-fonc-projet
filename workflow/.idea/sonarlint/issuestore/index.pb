4620714e14c9d4beaffd6454ba03c8825ff0a9f8
Mathias Santos Reis <mathias.santos-reis@etu.univ-cotedazur.fr>
2025-02-14 10:00:41 +0100

    databaseservice

---
+++ workflow/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+O
+src/main/java/project/Main.java,2\f\2f61cc679b7bac6c41274f18baa73f2d72b9bef1
+`
+0src/main/java/project/workflow/DataWorkflow.java,7\e\7e17781edf495c4cca2d09bf2217e1096ada445e
--- workflow/src/main/java/project/service/DatabaseService.java
+++ workflow/src/main/java/project/service/DatabaseService.java
@@ -1,4 +1,19 @@
 package project.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import project.model.UserRecord;
+
 public class DatabaseService {
+
+    private final List<UserRecord> database = new ArrayList<>();
+
+    public void saveUsers(List<UserRecord> users) {
+        database.addAll(users);
+        System.out.println(users.size() + " utilisateurs sauvegardés dans la base de données.");
+    }
+
+    public List<UserRecord> getAllUsers() {
+        return new ArrayList<>(database);
+    }
 }
